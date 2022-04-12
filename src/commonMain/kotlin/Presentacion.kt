65159c741e0f994fd07fdf36729bacd71fe5b3b8
Carla <ncargar010@g.educaand.es>
2022-04-12 18:18:04 +0200

    arreglado el alineamiento de los botones

--- src/commonMain/kotlin/Presentacion.kt
+++ src/commonMain/kotlin/Presentacion.kt
@@ -49,9 +49,9 @@ class Presentacion() : Scene() {
 
         var cuadroDialogo = text(conversacion.linea, textSize = 20.0, alignment = TextAlignment.MIDDLE_CENTER).centerXOnStage().alignBottomToTopOf(personaje, 20)
         var botones = container {}
-        var texto2 = text(conversacion.opcion2?.first ?: "" , alignment = TextAlignment.MIDDLE_CENTER).centerXOnStage().alignTopToBottomOf(personaje, 20)
-        var texto1 = text(conversacion.opcion1?.first ?: "" , alignment = TextAlignment.MIDDLE_CENTER).alignTopToBottomOf(personaje, 20).alignRightToLeftOf(texto2, 100)
-        var texto3 = text(conversacion.opcion3?.first ?: "" , alignment = TextAlignment.MIDDLE_CENTER) .alignTopToBottomOf(personaje, 20).alignLeftToRightOf(texto2, 100)
+        var texto2 = text(conversacion.opcion2?.first ?: "" , alignment = TextAlignment.MIDDLE_CENTER).alignTopToBottomOf(personaje, 30).positionX(views.virtualWidth/2)
+        var texto1 = text(conversacion.opcion1?.first ?: "" , alignment = TextAlignment.MIDDLE_CENTER).alignTopToBottomOf(personaje, 30).positionX(views.virtualWidth/2 - 120)
+        var texto3 = text(conversacion.opcion3?.first ?: "" , alignment = TextAlignment.MIDDLE_CENTER) .alignTopToBottomOf(personaje, 30).positionX(views.virtualWidth/2 + 120)
 
         fun actualizarTexto() {
             texto1.text = conversacion.opcion1!!.first
@@ -68,14 +68,23 @@ class Presentacion() : Scene() {
         var boton2 =
                 this.uiButton(width = 100.0, height = 50.0, text = "").centerOn(texto2)
                     .addTo(botones)
-                    .onClick { conversacion = conversacion.opcion2!!.second; cuadroDialogo.text = conversacion.linea; if (conversacion.opciones) { actualizarTexto() }  else {botones.removeChildren(); ocultarTexto()}}
+                    .onClick {
+                        conversacion = conversacion.opcion2!!.second
+                        cuadroDialogo.text = conversacion.linea
+                        if (conversacion.opciones) {actualizarTexto()}  else {botones.removeChildren(); ocultarTexto()}}
         var boton1 =
                 this.uiButton(width = 100.0, height = 50.0, text = "").centerOn(texto1)
                     .addTo(botones)
-                    .onClick { conversacion = conversacion.opcion1!!.second; cuadroDialogo.text = conversacion.linea;  if (conversacion.opciones) { actualizarTexto() } else {botones.removeChildren(); ocultarTexto()}}
+                    .onClick {
+                        conversacion = conversacion.opcion1!!.second
+                        cuadroDialogo.text = conversacion.linea
+                        if (conversacion.opciones) {actualizarTexto()} else {botones.removeChildren(); ocultarTexto()}}
         var boton3 =
                 this.uiButton(width = 100.0, height = 50.0, text = "").centerOn(texto3)
                     .addTo(botones)
-                    .onClick { conversacion = conversacion.opcion3!!.second; cuadroDialogo.text = conversacion.linea;  if (conversacion.opciones) { actualizarTexto() }  else {botones.removeChildren(); ocultarTexto()}}
+                    .onClick {
+                        conversacion = conversacion.opcion3!!.second
+                        cuadroDialogo.text = conversacion.linea
+                        if (conversacion.opciones) {actualizarTexto()}  else {botones.removeChildren(); ocultarTexto()}}
     }
 }
