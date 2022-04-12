a71b8900786231cc76b8ff4b81840ec6e0db29d9
Carla <ncargar010@g.educaand.es>
2022-04-12 19:09:57 +0200

    boton de entrar al juego cuando se termina la conversacion

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -3,12 +3,14 @@ import com.soywiz.korge.Korge
 suspend fun main() = Korge(Korge.Config(module = MyModule))
 
     /*
+
+    desde que meti el modulo no se donde meter esto lmao:
+
     Korge(
 	width = 480,
 	height = 640,
 	title="Anillo",
 	bgcolor =  Colors.ANTIQUEWHITE,
-    config = Korge(Korge.Config(module = MyModule))
 ) {
 
 } */
