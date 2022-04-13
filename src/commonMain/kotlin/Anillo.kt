349a9bb4fba581e4743ea9fbcf9601c2d2b29f1d
Carla <ncargar010@g.educaand.es>
2022-04-13 15:49:48 +0200

    el anillo ahora es un poco mas estetico

--- src/commonMain/kotlin/Anillo.kt
+++ src/commonMain/kotlin/Anillo.kt
@@ -23,28 +23,28 @@ class Anillo() : Scene() {
             //preparar lo necesario
             val total = cartas.size
             val mitad = total/2 - 1
-            val radio = views.virtualWidth/3
+            val radio = views.virtualWidth/4
             val centro = Pair(views.virtualWidth/2.0,views.virtualHeight/2.0)
             val angulo = 360.0/cartas.size * PI/180
             val rot = PI/2 + angulo/2
             val persp = 3
-            val ancho = 40.0
-            val alto = 60.0
-            val sep = ancho/2 + 5
+            val ancho = 40.0*3
+            val alto = 60.0*3
+            val sep = ancho*1.5/2 + 5
             var anillo = container { }
             var x: Double
             var y: Double
             if (cartas.size == 1) {
                 anillo.addChildren(listOf(
-                    RoundRect(width = ancho*1.1, height = alto*1.1, rx = 2.0, fill=Colors.GOLD, stroke=Colors.BLACK, strokeThickness = 2.0).center().position(centro.first, centro.second+radio /persp),
+                    RoundRect(width = ancho*1.5, height = alto*1.5, rx = 20.0, fill=Colors["#ff587f"], stroke=Colors["#a5253a"], strokeThickness = 2.0).center().position(centro.first, centro.second+radio /persp),
                     Text(text = cartas.removeFirst().toString(), alignment = TextAlignment.MIDDLE_CENTER, color = Colors.BLACK).position(centro.first, centro.second+radio /persp)
                 ))
                 return anillo
             }
             //si hay dos o más, colocamos las dos primeras en la posición central con una pequeña separación
             //el bucle se encargará del resto
-            val primeraCarta = RoundRect(width = ancho*1.1, height = alto*1.1, rx = 2.0, fill=Colors.GOLD, stroke=Colors.BLACK, strokeThickness = 2.0).center().position(centro.first-sep, centro.second+radio/persp)
-            val segundaCarta =	RoundRect(width = ancho*1.1, height = alto*1.1, rx = 2.0, fill=Colors.GOLD, stroke=Colors.BLACK, strokeThickness = 2.0).center().position(centro.first+sep, centro.second+radio/persp)
+            val primeraCarta = RoundRect(width = ancho*1.5, height = alto*1.5, rx = 20.0, fill=Colors["#ff587f"], stroke=Colors["#a5253a"], strokeThickness = 2.0).center().position(centro.first-sep, centro.second+radio/persp)
+            val segundaCarta =	RoundRect(width = ancho*1.5, height = alto*1.5, rx = 20.0, fill=Colors["#ff587f"], stroke=Colors["#a5253a"], strokeThickness = 2.0).center().position(centro.first+sep, centro.second+radio/persp)
             val primerTexto = Text(text = cartas.removeFirst().toString(), alignment = TextAlignment.MIDDLE_CENTER, color = Colors.BLACK).position(centro.first-sep, centro.second+radio/persp)
             val segundoTexto = Text(text = cartas.removeFirst().toString(), alignment = TextAlignment.MIDDLE_CENTER, color = Colors.BLACK).position(centro.first+sep, centro.second+radio/persp)
             //averiguar la posición de cada carta
@@ -53,13 +53,13 @@ class Anillo() : Scene() {
                 y = centro.second + radio*sin(angulo*i + rot)/persp
                 //añadir los elementos visuales de la carta al contenedor en esas posiciones
                 if (i <= mitad) {
-                    anillo.addChildAt(RoundRect(width = ancho, height = alto, rx = 2.0, fill=Colors.CORAL, stroke=Colors.BLACK, strokeThickness = 2.0).center().position(x,y), anillo.numChildren)
+                    anillo.addChildAt(RoundRect(width = ancho, height = alto, rx = 20.0, fill=Colors["#b181ce"], stroke=Colors["#a046b9"], strokeThickness = 2.0).center().position(x,y), anillo.numChildren)
                     anillo.addChildAt(Text(text = cartas.removeFirst().toString(), alignment = TextAlignment.MIDDLE_CENTER, color = Colors.BLACK).position(x, y), anillo.numChildren)
-                    anillo.addChildAt(RoundRect(width = ancho, height = alto, rx = 2.0, fill=Colors.BLACK, stroke=Colors.BLACK, strokeThickness = 2.0).center().position(x,y).alpha(i/total.toDouble()), anillo.numChildren)
+                    anillo.addChildAt(RoundRect(width = ancho, height = alto, rx = 20.0, fill=Colors.BLACK, stroke=Colors.BLACK, strokeThickness = 2.0).center().position(x,y).alpha(i/total.toDouble()), anillo.numChildren)
                 } else {
-                    anillo.addChildAt(RoundRect(width = ancho, height = alto, rx = 2.0, fill=Colors.BLACK, stroke=Colors.BLACK, strokeThickness = 2.0).center().position(x,y).alpha(1 - i/total.toDouble()), 0)
+                    anillo.addChildAt(RoundRect(width = ancho, height = alto, rx = 20.0, fill=Colors.BLACK, stroke=Colors.BLACK, strokeThickness = 2.0).center().position(x,y).alpha(1 - i/total.toDouble()), 0)
                     anillo.addChildAt(Text(text = cartas.removeFirst().toString(), alignment = TextAlignment.MIDDLE_CENTER, color = Colors.BLACK).position(x, y), 0)
-                    anillo.addChildAt(RoundRect(width = ancho, height = alto, rx = 2.0, fill=Colors.CORAL, stroke=Colors.BLACK, strokeThickness = 2.0).center().position(x,y), 0)
+                    anillo.addChildAt(RoundRect(width = ancho, height = alto, rx = 20.0, fill=Colors["#b181ce"], stroke=Colors["#a046b9"], strokeThickness = 2.0).center().position(x,y), 0)
                 }
             }
             anillo.addChildren(listOf(primeraCarta,segundaCarta,primerTexto,segundoTexto))
@@ -82,3 +82,8 @@ class Anillo() : Scene() {
         contenedorPrincipal.addChild(botones)
     }
 }
+
+/* TODO:
+-Falta mover el anillo hacia arriba para que quepan los demás elementos de la interfaz.
+-Falta crear el resto de los elementos de la interfaz.
+ */
